0633ce29191d8cf538f1c586954ad45446ddbe9b
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-05-03 08:31:43 +0200

    code cleanup, added possible improvements in readme

--- ctest.h
+++ ctest.h
@@ -124,23 +124,23 @@ static const char* suite_name;
 
 typedef int (*filter_func)(struct ctest*);
 
-#define ANSI_BLACK "\033[0;30m"
-#define ANSI_RED "\033[0;31m"
-#define ANSI_GREEN "\033[0;32m"
-#define ANSI_YELLOW "\033[0;33m"
-#define ANSI_BLUE "\033[0;34m"
-#define ANSI_MAGENTA "\033[0;35m"
-#define ANSI_CYAN "\033[0;36m"
-#define ANSI_GREY "\033[0;37m"
+#define ANSI_BLACK    "\033[0;30m"
+#define ANSI_RED      "\033[0;31m"
+#define ANSI_GREEN    "\033[0;32m"
+#define ANSI_YELLOW   "\033[0;33m"
+#define ANSI_BLUE     "\033[0;34m"
+#define ANSI_MAGENTA  "\033[0;35m"
+#define ANSI_CYAN     "\033[0;36m"
+#define ANSI_GREY     "\033[0;37m"
 #define ANSI_DARKGREY "\033[01;30m"
-#define ANSI_BRED "\033[01;31m"
-#define ANSI_BGREEN "\033[01;32m"
-#define ANSI_BYELLOW "\033[01;33m"
-#define ANSI_BBLUE "\033[01;34m"
+#define ANSI_BRED     "\033[01;31m"
+#define ANSI_BGREEN   "\033[01;32m"
+#define ANSI_BYELLOW  "\033[01;33m"
+#define ANSI_BBLUE    "\033[01;34m"
 #define ANSI_BMAGENTA "\033[01;35m"
-#define ANSI_BCYAN "\033[01;36m"
-#define ANSI_WHITE "\033[01;37m"
-#define ANSI_NORMAL "\033[0m"
+#define ANSI_BCYAN    "\033[01;36m"
+#define ANSI_WHITE    "\033[01;37m"
+#define ANSI_NORMAL   "\033[0m"
 
 static CTEST(suite, test) { }
 
