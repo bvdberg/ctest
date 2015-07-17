0f7e32599fbe58dba099aed6dc5aa3a424d83739
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2015-07-17 08:55:35 +0200

    print note to say some tests are supposed to fail in example

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define CTEST_MAIN
 // uncomment line below to get nicer logging on segfaults
 #define CTEST_SEGFAULT
@@ -5,6 +7,9 @@
 
 int main(int argc, const char *argv[])
 {
-    return ctest_main(argc, argv);
+    int result = ctest_main(argc, argv);
+
+    printf("\nNOTE: some tests will fail, just to show how ctest works! ;)\n");
+    return result;
 }
 
