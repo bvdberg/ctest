0f3f58018cc6db8f31858ba603193879484fcbf1
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2018-04-04 21:44:12 +0200

    example: removed usleep since it seems to cause issues on Windows

--- mytests.c
+++ mytests.c
@@ -1,10 +1,8 @@
-#include <unistd.h>
 #include <stdlib.h>
 #include "ctest.h"
 
 // basic test without setup/teardown
 CTEST(suite1, test1) {
-    usleep(2000);
 }
 
 // there are many different ASSERT macro's (see ctest.h)
